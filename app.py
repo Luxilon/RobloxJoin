bcb29b04fd1c28c32e9ef63fd09c85dccfcc28fa
h4zey <mangowalk04@gmail.com>
2019-03-26 19:58:39 +0000

    Update app.py

--- app.py
+++ app.py
@@ -8,14 +8,12 @@ from os import path
 from subprocess import Popen
 from urllib.parse import quote
 
-COOKIE = "ENTER_ROBLOX_COOKIE_HERE"
+ROBLOX_SEC = "ENTER_ROBLOSECURITY_HERE"
 
 class RobloxAPI:
     def __init__(self, cookie):
         self.sess = requests.Session()
-        self.sess.headers.update({
-            "Cookie": cookie
-        })
+        self.sess.cookies[".ROBLOSECURITY"] = cookie
 
         # uncomment underneath line if you have ssl issues
         # self.sess.verify = False 
@@ -36,7 +34,7 @@ class RobloxAPI:
         return resp.json()
 
     def game_authentication(self, place_id) -> str:
-        resp = self.sess.get("https://www.roblox.com/game-auth/getauthticket", headers={ "RBX-For-Gameauth": "true", 
+        resp = self.sess.get("https://www.roblox.com/game-auth/getauthticket", headers={ "RBX-For-Gameauth": "true",
                                                                                          "Referer": "https://www.roblox.com/games/{game_id}" })
         return resp.text
 
@@ -44,7 +42,7 @@ class RobloxAPI:
         kwargs.update({ "headers": { "X-CSRF-TOKEN": self.csrf_token() } })
         return self.sess.post(url, data, json, **kwargs)
 
-    def get_user_presence(self, *args) -> list:
+    def get_user_presence(self, *args) -> dict:
         return self.post_with_token("https://presence.roblox.com/v1/presence/users", json={ "userIds": args }).json()
 
 
@@ -58,7 +56,7 @@ def user_presence(rb, uid) -> dict:
 
 def player_path() -> str:  
     version = RobloxAPI.player_version()
-    return path.expandvars(f"%LOCALAPPDATA%\\Roblox\\Versions\\{version}\\RobloxPlayerLauncher.exe")
+    return path.expandvars(f"%LOCALAPPDATA%\\Roblox\\Versions\\{version}\\RobloxPlayerLauncher_main.exe")
 
 def load_player(auth, place_id, game_id):
     url = quote(f"https://assetgame.roblox.com/game/PlaceLauncher.ashx?request=RequestGameJob&placeId={place_id}&gameId={game_id}")
@@ -68,9 +66,9 @@ def load_player(auth, place_id, game_id):
         Popen([ player_path(), arg ], shell=False)
     except FileNotFoundError:
         sys.exit("Error: latest version of Roblox not found")
-        
+
 def init():
-    rb = RobloxAPI(COOKIE)
+    rb = RobloxAPI(ROBLOX_SEC)
     user = input("Enter username: ")
 
     uid = rb.username_info(user).get("Id")
@@ -87,7 +85,7 @@ def init():
         if user["userPresenceType"] == 2:
             auth = rb.game_authentication(place_id)
             load_player(auth, place_id, game_id)
-            
+
             break
 
 if __name__ == "__main__":
